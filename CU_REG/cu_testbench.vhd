3fb5f3bffa6f65e76208d5777301f615b542b5b0
Pat <geeks_kick@yahoo.co.uk>
2018-06-10 17:28:42 +0100

    CU Test bench STAC and NOP added

--- CU_REG/cu_testbench.vhd
+++ CU_REG/cu_testbench.vhd
@@ -73,6 +73,7 @@ stim:
         assert MEMBUS = '1' report "MEMBUS incorrect in FETCH2: " & std_logic'image(MEMBUS);
         assert PCINC = '1' report "PCINC";
         assert DRLOAD = '1' report "DRLOAD";
+        assert R = '1' report "R";
          
         -- FETCH3
         wait for period;
@@ -90,6 +91,7 @@ stim:
         report "LDAC1";
         assert DRLOAD = '1' report "DRLOAD";
         assert MEMBUS = '1' report "MEMBUS";
+        assert R = '1' report "R";
         assert PCINC = '1' report "PCINC";
         assert ARINC = '1' report "ARINC";
         
@@ -99,6 +101,7 @@ stim:
         assert TRLOAD = '1' report "TRLOAD";
         assert DRLOAD = '1' report "DRLOAD";
         assert MEMBUS = '1' report "MEMBUS";
+        assert R = '1' report "R";
         assert PCINC = '1' report "PCINC";
         
         -- LDAC 3
@@ -113,6 +116,7 @@ stim:
         report "LDAC4";
         assert DRLOAD = '1' report "DRLOAD";
         assert MEMBUS = '1' report "MEMBUS";
+        assert R = '1' report "R";
         
         --LDAC 5
         wait for period;
@@ -121,11 +125,77 @@ stim:
         assert DRLBUS = '1' report "DRLBUS";
         
         --FETCH1
+        ir <= "00000";
         wait for period;
         report "FETCH1";
         assert PCBUS = '1' report "PCBUS incorrect in FETCH1: " & std_logic'image(PCBUS);
         assert ARLOAD = '1' report "ARLOAD incorrect in FETCH1: " & std_logic'image(ARLOAD);
         
+        wait for period;
+        report "FETCH2";
+        
+        wait for period;
+        report "FETCH3";
+        
+        wait for period;
+        report "NOP";
+        assert error = '0' and ARLOAD = '0' and ARINC= '0' and R= '0' and W= '0' and MEMBUS= '0' and BUSMEM= '0' and PCINC= '0' and PCLOAD= '0' and PCBUS= '0' and DRLBUS= '0' and DRHBUS= '0' and DRLOAD= '0' and TRLOAD= '0' and TRBUS= '0' and IRLOAD= '0' and RLOAD= '0' and RBUS= '0' and ACLOAD= '0' and ACBUS = '0' and ZLOAD = '0';
+        
+        --FETCH1
+        ir <= "00010";
+        wait for period;
+        report "FETCH1";
+        assert PCBUS = '1' report "PCBUS incorrect in FETCH1: " & std_logic'image(PCBUS);
+        assert ARLOAD = '1' report "ARLOAD incorrect in FETCH1: " & std_logic'image(ARLOAD);
+        
+        wait for period;
+        report "FETCH2";
+        
+        wait for period;
+        report "FETCH3";
+        
+        wait for period;
+        report "STAC0";
+        
+        wait for period;
+        report "STAC1";
+        assert DRLOAD = '1' report "DRLOAD";
+        assert MEMBUS = '1' report "MEMBUS";
+        assert R = '1' report "R";
+        assert PCINC = '1' report "PCINC";
+        assert ARINC = '1' report "ARINC";
+        
+        wait for period;
+        report "STAC2";
+        assert TRLOAD = '1' report "TRLOAD";
+        assert DRLOAD = '1' report "DRLOAD";
+        assert MEMBUS = '1' report "MEMBUS";
+        assert R = '1' report "R";
+        assert PCINC = '1' report "PCINC";
+        
+        wait for period;
+        report "STAC3";
+        assert ARLOAD = '1' report "ARLOAD";
+        assert TRBUS = '1' report "TRBUS";
+        assert DRHBUS = '1' report "DRHBUS";
+        
+        wait for period;
+        report "STAC4";
+        assert DRLOAD = '1' report "DRLOAD";
+        assert ACBUS = '1' report "ACBUS";
+        
+        wait for period;
+        report "STAC5";
+        assert DRLBUS = '1' report "DRLBUS";
+        assert BUSMEM = '1' report "BUSMEM";
+        assert W = '1' report "W";
+        
+        --FETCH1
+        ir <= "00011";
+        wait for period;
+        report "FETCH1";
+        assert PCBUS = '1' report "PCBUS incorrect in FETCH1: " & std_logic'image(PCBUS);
+        assert ARLOAD = '1' report "ARLOAD incorrect in FETCH1: " & std_logic'image(ARLOAD);
         stop <= true;
         wait;
     end process;
